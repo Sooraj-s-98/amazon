7084f297ca2425a07d1e7d1d401023dd3261aafe
sooraj_s <sooraj1998ayiroor@gmail.com>
2020-08-07 20:28:13 +0530

    initial

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import './App.css';
+import { BrowserRouter as Router,Switch,Route } from 'react-dom';
+
+function App() {
+  return (
+    <Router>
+    <div className="App">
+      <Switch>
+         <Route  path="/checkout">
+            <h1>checkout</h1>
+         </Route>
+
+         <Route  path="/login">
+            <h1>login page</h1>
+         </Route>
+
+         <Route  path="/">
+            <h1>Home page</h1>
+         </Route>
+      </Switch>
+    </div>
+    </Router>
+
+  );
+}
+
+export default App;
