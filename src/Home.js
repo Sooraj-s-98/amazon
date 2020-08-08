c9d28fc690968e2be3fd42909d4b6563a8c4a0b2
sooraj_s <sooraj1998ayiroor@gmail.com>
2020-08-08 20:51:34 +0530

    product test

--- src/Home.js
+++ src/Home.js
@@ -1,11 +1,21 @@
 import React from 'react'
 import './Home.css';
-
+import  Product from "./Product";
 function Home() {
     return (
         <div className="home">
             <img className="home_image" src="https://images-eu.ssl-images-amazon.com/images/G/02/digital/video/merch2016/Hero/Covid19/Generic/GWBleedingHero_ENG_COVIDUPDATE__XSite_1500x600_PV_en-GB._CB428684220_.jpg" alt=""/>
-        </div>
+            <div className="home_row">
+              <Product
+               id="2"
+               title="OPPO Reno 2Z (Luminous Black, 8GB RAM, 256GB Storage) with No Cost EMI/Additional Exchange Offers"
+               price={100}
+               rating={5}
+               image="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ4j5AFKaxHRFi9r_HREbuXIPIhSopndZ7uLVYfW4Rg6ioF6ig&s"
+              />
+            </div>
+ 
+       </div>
     )
 }
 
